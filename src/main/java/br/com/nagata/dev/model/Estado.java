ecc34f15712980efac820f2528026b1aa7e5cbea
Tiago Massashi Nagata <59301876+tiagomassashi@users.noreply.github.com>
2022-03-02 02:49:49 -0300

    feat(*): Estado e Cidade

---
+++ src/main/java/br/com/nagata/dev/model/Estado.java
@@ -0,0 +1,34 @@
+package br.com.nagata.dev.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Estado implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+  private String nome;
+
+  @OneToMany(mappedBy = "estado")
+  private List<Cidade> cidades = new ArrayList<>();
+}
